d8018a05c9db289e5ae46413fe6af3223b36a8c8
Zekios <joserafaelleticia2018@gmail.com>
2025-09-15 11:15:17 -0300

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Loja from "./components/Loja";
